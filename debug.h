a95a3433cb9ee3c24402893d68a5d1a61af68788
Guangyu Sun <guangyu.sun@oracle.com>
2013-09-23 21:46:20 -0700

    add gsWarn() function
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- debug.h
+++ debug.h
@@ -12,6 +12,11 @@ inline void gsBug(const char* msg)
     exit(1);
 }
 
+inline void gsWarn(const char* msg)
+{
+    std::cerr << "Error: " << msg << std::endl;
+}
+
 } // namespace gs
 
 #endif // GS_DEBUG_H
